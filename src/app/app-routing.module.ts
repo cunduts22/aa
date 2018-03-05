40d5630f90ee20dba8de301b1d405fedec60ea02
Mona Lisacunduts <yusrilsabir22@gmail.com>
2018-03-05 14:36:30 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ArtikelComponent } from './artikel/artikel.component';
+
+const routes: Routes = [
+  {
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'home'
+  },
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+    path: 'artikel',
+    component: ArtikelComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ArtikelComponent } from './artikel/artikel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ArtikelComponent
   ],
   imports: [
     BrowserModule,
